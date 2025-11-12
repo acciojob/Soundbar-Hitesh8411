aa82c52b6c342d85cc50942bdb8ed23338027f07
Hitesh Suryavanshi <167613770+Hitesh8411@users.noreply.github.com>
2025-11-12 22:42:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+sounds.forEach(sound => {
+  const audio = new Audio(`sounds/${sound}.mp3`);
+
+  const btn = document.querySelector(`.btn:nth-child(${sounds.indexOf(sound) + 1})`);
+
+  btn.addEventListener('click', () => {
+    stopSounds();
+    audio.play();
+  });
+});
+
+document.querySelector('.stop').addEventListener('click', stopSounds);
+
+function stopSounds() {
+  sounds.forEach(sound => {
+    const audio = new Audio(`sounds/${sound}.mp3`);
+    audio.pause();
+    audio.currentTime = 0;
+  });
+}
